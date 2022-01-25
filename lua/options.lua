4073912f7d808f1511af0771d437d722fc29179e
Wiktor Czyż <czyz.wiktor@gmail.com>
2022-01-25 02:15:05 +0100

    Set mapleader to space and disable line wrapping

--- lua/options.lua
+++ lua/options.lua
@@ -8,6 +8,8 @@ vim.cmd "syntax on"
 o.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50" -- block in normal and beam cursor in insert mode
 vim.cmd "colorscheme nightfox"
 
+vim.g.mapleader = " "   -- set leader to space
+
 o.updatetime = 300	    -- faster completion
 o.timeoutlen = 400  	-- time to wait for a mapped sequence to complete (in milliseconds)
 o.ttimeoutlen = 0   	-- time in milliseconds to wait for a key code sequence to complete
@@ -22,6 +24,7 @@ o.splitbelow = true 	-- force all horizontal split to show on the bottom
 o.termguicolors = true 	-- enable term gui colors
 o.number = true         -- set numbered lines
 o.completeopt = {"menu", "menuone", "noselect", "noinsert"} -- comma separated list of options for Insert mode completion
+o.wrap = false         -- don't wrap lines
 
 
 wo.colorcolumn = "120"  -- show column width line at 120 characters
