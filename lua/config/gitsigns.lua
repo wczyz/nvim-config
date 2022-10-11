5fc93b77ff32ba146e98f89b5a6b821089ae92bb
Wiktor Czyż <czyz.wiktor@gmail.com>
2022-10-11 02:15:01 +0200

    Update field name, from watch_index to watch_gitdir

--- lua/config/gitsigns.lua
+++ lua/config/gitsigns.lua
@@ -39,7 +39,7 @@ require('gitsigns').setup {
     numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
     linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
     word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
-    watch_index = {interval = 1000, follow_files = true},
+    watch_gitdir = {interval = 1000, follow_files = true},
     attach_to_untracked = true,
     -- git-blame provides also the time in contrast to gitsigns
     current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
